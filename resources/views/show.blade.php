d92fca91e8f2955971818fcccaf75fe8b8257ecc
Bous15 <115042101+belakebiboussad@users.noreply.github.com>
2025-09-30 09:26:56 +0000

    change codespace

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -24,7 +24,7 @@
     <div class="row">
         <div class="col-md-8">
             <h1>{{ $post->title }}</h1>
-
+<!-- comment  -->
             <p class="lead">{!! $post->body !!}</p>
         </div>
         <div class="col-md-4">
